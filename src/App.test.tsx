6cde5f14784de897c4e0bed9b6156a8535d3751c
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2022-02-06 00:10:46 +0100

    feat: begin oidc integration (wip)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const iotakingdomsTextElement = screen.getByText(/iotakingdoms/i);
+  expect(iotakingdomsTextElement).toBeInTheDocument();
+  const loginLinkElement = screen.getByText(/Login/i);
+  expect(loginLinkElement).toBeInTheDocument();
 });
--- src/Start.tsx
+++ src/Start.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 
 export default function Start() {
+  const authLink = 'http://auth.iotakingdoms.com/auth?response_type=code&client_id=ik-p-app&scope=openid&redirect_uri=http://app.iotakingdoms.com/cb';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          iotakingdoms
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={authLink}
         >
-          Learn React
+          Login
         </a>
       </header>
     </div>
